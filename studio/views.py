ad6b2b9860c91a5ed121e849bc55a7c7b6aa9c8c
TahaaFatima <tahafatima95@gmail.com>
2020-03-04 20:01:48 +0500

    completed

--- studio/views.py
+++ studio/views.py
@@ -29,36 +29,51 @@ def contact(request):
 
 def portfolio(request):
     return render(request, 'portfolio.html')
+empty = 'ds'
+
 
 def books(request):
+    global empty
     if request.method == 'POST' and request.FILES['myfile']:
+        global empty
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
         uploaded_file_url = fs.url(filename)
         user_data = pd.read_csv(myfile.name)
         column_data = user_data.columns
+        values_data = user_data.values
+        empty = uploaded_file_url
+        # print(empty)
         
         context = {
             'uploaded_file_url': uploaded_file_url,
             'user_data'        : user_data,
-            'column_data'      : column_data
+            'column_data'      : column_data,
+            'values_data' : values_data
         }
         return render(request, 'books.html', context)
     return render(request, 'books.html')
 
 def books_two(request):
+    global empty
+    print(empty)
+
     if request.method == 'POST':
-        user_data   = pd.read_csv('books.csv')
-        column_data = user_data.columns
+        csv_col_names = request.POST.getlist('csv_col_names')
+        print(empty)
+        print(f'./{empty}')
+        user_data   = pd.read_csv(f'./{empty}', usecols=csv_col_names)
+
+        # column_data = user_data.columns
         # csv_col_names = request.POST.getlist('csv_col_names')
-        main_data = user_data[['age']]
+        # main_data = user_data[['age']]
         
         context = {
             # 'uploaded_file_url': uploaded_file_url,
-            'user_data'        : user_data,
-            'column_data'      : column_data,
-            'main-data' : main_data
+            # 'user_data'        : user_data,
+            # 'column_data'      : column_data
+            'main_data' : user_data,
         }
         return render(request, 'books.html', context)
 
